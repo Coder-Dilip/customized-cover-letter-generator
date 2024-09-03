8e212b35894a44209a592f95a2d6c10e7632a40e
coder-dilip <designsdilip@gmail.com>
2024-09-03 23:20:59 +0545

    added docker configurations

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import chromadb
 import uuid
-
+import os
 
 class Portfolio:
     def __init__(self, file_path="./resource/my_portfolio.csv"):
-        self.file_path = file_path
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(base_dir, "./resource/my_portfolio.csv")
         self.data = pd.read_csv(file_path)
         self.chroma_client = chromadb.PersistentClient('vectorstore')
         self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
